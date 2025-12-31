c8d07af7ddadebdeba6ec07c11145cbecb73570a
Ayrton Caio <caiodalmune@gmail.com>
2025-12-31 19:14:17 -0300

    hotfix

--- src/components/searchInput/index.jsx
+++ src/components/searchInput/index.jsx
@@ -101,7 +101,7 @@ const SearchInput = () => {
                                         {
                                             users.map((user, index) => {
                                                 return (
-                                                    <div style={index == 0 ? { borderTopLeftRadius: '15px', borderTopRightRadius: '15px', paddingTop: '18px' } : { borderTopLeftRadius: '0px', borderTopRightRadius: '0px' }} onClick={() => navigate(`/profile/${user.user}`)} className="hover:bg-[#30005bff] flex cursor-pointer items-center p-4 pb-3 pt-3" key={index}>
+                                                    <div style={index == 0 ? { borderTopLeftRadius: '15px', borderTopRightRadius: '15px', paddingTop: '18px' } : { borderTopLeftRadius: '0px', borderTopRightRadius: '0px' }} onClick={() => navigate(`/profile/${user.user}`)} className="hover:bg-[#30005bff] flex cursor-pointer items-start p-4 pb-3 pt-3" key={index}>
                                                         <img className="w-[45px] h-[45px] rounded-[50%]" src={user.profileImg == null ? ImageProfile : user.profileImg} alt="" />
 
                                                         <div className="flex flex-col ml-2">
